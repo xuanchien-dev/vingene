b309993a9ca64edd6684daa14794978190d6f50b
Fuckingwowshhh <vietlv.ttv@gmail.com>
2020-07-06 17:28:39 +0700

    Trang

--- js/default-assets/custom-tr.js
+++ js/default-assets/custom-tr.js
@@ -10,4 +10,38 @@ $(document).ready(function () {
         $(".price-tabs-show3").fadeToggle("slow");
     });
 
+
+
 });
+/*
+$('.dc-top2').click(function(event) {
+    event.preventDefault();
+    $('.phieuketqua').css('display', 'block');
+});*/
+
+$('.button-close').click(function(event) {
+    event.preventDefault();
+    $('.phieuketqua').css('display', 'none');
+});
+
+
+$('.button-close').click(function(event) {
+    event.preventDefault();
+    $('#dangkidichvu').css('display', 'none');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
